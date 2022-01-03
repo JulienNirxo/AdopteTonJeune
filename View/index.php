24471fc36c3a6a3326f2311a3f72887398fb7718
JulienNirxo <67024385+JulienNirxo@users.noreply.github.com>
2022-01-03 11:02:05 +0100

    design

--- View/index.php
+++ View/index.php
@@ -19,16 +19,15 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#page-top">Adopte Ton Jeune</a>
+                <a class="navbar-brand" href="#page-top"><img src="View/assets/img/logo.jpg">Adopte Ton Jeune</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#signup">Contact</a></li>
+                        <li class="nav-item"><a class="btn btn-primary" href="#about">Inscription</a></li>
+                        <li class="nav-item"><a class="btn btn-primary" href="#about">Connexion</a></li>
                     </ul>
                 </div>
             </div>
