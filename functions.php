23fa31d3e4a804ee453dfd12f64ff1cfdc138f2f
JulienNirxo <67024385+JulienNirxo@users.noreply.github.com>
2022-02-01 11:11:55 +0100

    modif depart

--- functions.php
+++ functions.php
@@ -1,10 +1,17 @@
 <?php
 require_once('Model/DAOcompte.php');
 $connect = new DAOcompte();
+
+//inscription de l'utilisateur
 if(isset($_POST['inscription'])) {
     if($_POST['mdp2'] == $_POST['mdp']){
         $_POST['mdp'] = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
         //NOM, PRENOM, AGE, ADRESSE, VILLE, CP, MAIL, PASSWORD
         $connect->adduser($_POST['nom'], $_POST['prenom'], $_POST['age'], $_POST['adresse'], $_POST['ville'], $_POST['cp'], $_POST['email'], $_POST['mdp'], $_POST['type']);
     }
+}
+
+//recherche des départements
+if(isset($_POST['cities'])){
+
 }
